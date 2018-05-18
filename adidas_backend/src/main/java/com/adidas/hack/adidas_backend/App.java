4964989035d5ffb830288bf658ac855a9e0d07d9
Fernando Ortega <f.ortega.diez@gmail.com>
2018-05-18 23:07:33 +0200

    test 1

---
+++ adidas_backend/src/main/java/com/adidas/hack/adidas_backend/App.java
@@ -0,0 +1,13 @@
+package com.adidas.hack.adidas_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
